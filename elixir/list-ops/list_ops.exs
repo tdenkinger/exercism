1b90e7ff32417bedcb5e555866fa1740ecfcd87d
Troy Denkinger <tdenkinger@gmail.com>
2016-06-03 08:01:38 -0400

    Implement the 'MyMap' module that handles mapping functions. Renamed the other modules to prefix 'My' to avoid any name collision with built-in modules.

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -7,15 +7,13 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l), do: Count.count(l, 0)
+  def count(l), do: MyCount.count(l, 0)
 
   @spec reverse(list) :: list
-  def reverse(l), do: Reverse.reverse(l)
+  def reverse(l), do: MyReverse.reverse(l)
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-
-  end
+  def map(l, f), do: MyMap.map(l, f)
 
   @spec filter(list, (any -> as_boolean(term))) :: list
   def filter(l, f) do
@@ -39,14 +37,21 @@ defmodule ListOps do
   end
 end
 
-defmodule Reverse do
+defmodule MyMap do
+  def map([], _), do: []
+  def map(list, fun), do: map(list, fun, [])
+  def map([h | t], fun, list), do: map(t, fun, [fun.(h) | list])
+  def map([], _, list), do: MyReverse.reverse(list)
+end
+
+defmodule MyReverse do
   def reverse([]),            do: []
   def reverse(list),          do: reverse(list, [])
   def reverse([h | t], list), do: reverse(t, [h | list])
   def reverse([], list),      do: list
 end
 
-defmodule Count do
+defmodule MyCount do
   def count([], acc),      do: acc
   def count([h | t], acc), do: count(t, acc + 1)
 end
--- elixir/list-ops/list_ops_test.exs
+++ elixir/list-ops/list_ops_test.exs
@@ -39,22 +39,22 @@ defmodule ListOpsTest do
     assert L.reverse([1,3,5,7]) == [7,5,3,1]
   end
 
-  @tag :pending
+  # @tag :pending
   test "reverse of huge list" do
     assert L.reverse(Enum.to_list(1..1_000_000)) == Enum.to_list(1_000_000..1)
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of empty list" do
     assert L.map([], &(&1+1)) == []
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of normal list" do
     assert L.map([1,3,5,7], &(&1+1)) == [2,4,6,8]
   end
 
-  @tag :pending
+  # @tag :pending
   test "map of huge list" do
     assert L.map(Enum.to_list(1..1_000_000), &(&1+1)) ==
       Enum.to_list(2..1_000_001)
